b90992eb8b357bfa373da823a0cdea382504943e
Koliva <paulkolivatzis@gmail.com>
2025-09-08 17:33:54 +0300

    Expose adapters and plugins

--- orchestrator/adapters/__init__.py
+++ orchestrator/adapters/__init__.py
@@ -1,1 +1,13 @@
 """Adapter registry for contract-based routing."""
+
+from .slot2_deltathresh import Slot2DeltaThreshAdapter
+from .slot8_memory_ethics import Slot8MemoryEthicsAdapter
+from .slot9_distortion_protection import Slot9DistortionProtectionAdapter
+from .slot10_civilizational import Slot10DeploymentAdapter
+
+__all__ = [
+    "Slot2DeltaThreshAdapter",
+    "Slot8MemoryEthicsAdapter",
+    "Slot9DistortionProtectionAdapter",
+    "Slot10DeploymentAdapter",
+]
--- orchestrator/plugins/__init__.py
+++ orchestrator/plugins/__init__.py
@@ -1,1 +1,6 @@
 """Plugin system for NOVA slots."""
+
+from .filepython import PythonFilePlugin
+from .rest import RestAPIPlugin
+
+__all__ = ["PythonFilePlugin", "RestAPIPlugin"]
